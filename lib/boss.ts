d0462108a8cb8eb6e9930fb16babc61335a37764
Briuwu <millontebrian@gmail.com>
2024-11-17 20:27:41 +0800

    Add dynamic boss image dimensions based on boss type; refactor width and height assignments

--- lib/boss.ts
+++ lib/boss.ts
@@ -5,32 +5,82 @@ export const getBoss = (
   size: number,
   bossName: string,
 ) => {
+  let idle_width,
+    idle_height,
+    hurt_width,
+    hurt_height,
+    death_width,
+    death_height,
+    attack_width,
+    attack_height,
+    walk_width,
+    walk_height;
+
   const boss = bossName.toLowerCase();
+
+  switch (boss) {
+    case "bloated":
+      attack_width = 324;
+      attack_height = 325;
+      death_width = 314;
+      death_height = 319;
+      hurt_width = 289;
+      hurt_height = 310;
+      idle_width = 301;
+      idle_height = 328;
+      walk_width = 313;
+      walk_height = 322;
+      break;
+    case "centipede":
+      attack_width = 426;
+      attack_height = 276;
+      death_width = 392;
+      death_height = 240;
+      hurt_width = 299;
+      hurt_height = 239;
+      idle_width = 332;
+      idle_height = 247;
+      walk_width = 332;
+      walk_height = 247;
+      break;
+    case "turtle":
+      attack_width = 423;
+      attack_height = 254;
+      death_width = 424;
+      death_height = 267;
+      hurt_width = 370;
+      hurt_height = 250;
+      idle_width = 393;
+      idle_height = 249;
+      walk_width = 396;
+      walk_height = 271;
+      break;
+  }
   const images = {
     idle: {
       src: `/boss/${boss}/idle.gif`,
-      width: 332,
-      height: 247,
+      width: idle_width ?? 0,
+      height: idle_height ?? 0,
     },
     hurt: {
       src: `/boss/${boss}/hurt.gif`,
-      width: 299,
-      height: 239,
+      width: hurt_width ?? 0,
+      height: hurt_height ?? 0,
     },
     death: {
       src: `/boss/${boss}/death.gif`,
-      width: 392,
-      height: 240,
+      width: death_width ?? 0,
+      height: death_height ?? 0,
     },
     attack: {
       src: `/boss/${boss}/attack.gif`,
-      width: 426,
-      height: 276,
+      width: attack_width ?? 0,
+      height: attack_height ?? 0,
     },
     walk: {
       src: `/boss/${boss}/walk.gif`,
-      width: 332,
-      height: 247,
+      width: walk_width ?? 0,
+      height: walk_height ?? 0,
     },
   };
 
