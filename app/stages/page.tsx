a890f1461922535fb691765f75be2b54316d2716
Briuwu <millontebrian@gmail.com>
2024-11-27 21:39:39 +0800

    add

--- app/stages/page.tsx
+++ app/stages/page.tsx
@@ -31,7 +31,7 @@ const StagesPage = async () => {
   return (
     <main>
       <AudioPlayer audioId={2} />
-      <h1 className="my-5 text-center font-bold uppercase text-red-500 md:text-3xl">
+      <h1 className="x`font-bold my-5 text-center uppercase text-red-500 md:text-3xl">
         All Stages are unlocked for testing purposes. Please select a stage to
         play.
       </h1>
