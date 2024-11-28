01563d6e432a20b625e6a5100b7fdb0821cc8d44
Briuwu <millontebrian@gmail.com>
2024-11-28 10:21:25 +0800

    Update socket connection URL based on environment; use production URL if available

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,4 +1,9 @@
 "use client";
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001");
+const url =
+  process.env.NODE_ENV === "production"
+    ? process.env.WEBSITE
+    : "http://localhost:3001";
+
+export const socket = io(url!);
